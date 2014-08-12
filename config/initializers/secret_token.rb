b667d20504e69b4966569b70e5c8074831c9af72
Yevhenii Kurtov <eugene.ku@randrmusic.com>
2014-08-12 16:08:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MelowApi::Application.config.secret_token = '8c22e79ab2806f45df9889ff014f7f109fac0d597f42a44259140175545bac77afba267b23185c47cd58359663281301d8c225e4b68a4800f9e08c2ac83ee987'
